43e9a57579112ecd3075fcc2506ca0674e95974f
UdayKiranSure <sureudaykiran1@gmail.com>
2026-01-27 21:48:01 +0530

    changes in init method

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,13 @@
 import tiktoken 
 from config import SwitchTransformerConfig
 class Dataloader():
-
-    def __init__(self, config, file_path):
+    def __init__(self, config):
         self.batch_size = config.batch_size
         self.seq_len = config.seq_len
         self.device = config.device
-        self.tokens = self._load_and_Tokenize(file_path)
+        self.tokens = self._load_and_Tokenize(config.file_path)
         self.current_pos = 0
+        self.steps = len(self.tokens)//(self.batch_size*self.seq_len)
 
     def _load_and_Tokenize(self, file_path):
         with open(file_path, 'r') as f:
@@ -16,10 +16,9 @@ class Dataloader():
         tokens = enc.encode(text)
         tokens = torch.tensor(tokens, device = self.device)
         return tokens
-        
+    
     def next_batch(self):
         B,T = self.batch_size[0], self.seq_len[0]
-        print(B,T)
         buf = self.tokens[self.current_pos: self.current_pos+ B*T + 1]
         x = buf[:-1].view(B,T)
         y = buf[1:].view(B,T)
@@ -28,9 +27,11 @@ class Dataloader():
             self.current_pos = 0
         return x,y
 
-config = SwitchTransformerConfig(1,1,1,16,8,'cpu')
-dataloader = Dataloader(config, 'shakespere.txt')
-config.batch_size[0]
-x,y = dataloader.next_batch()
-x.shape, y.shape
+
+
+# config = SwitchTransformerConfig(1,1,1,16,8,'cpu')
+# dataloader = Dataloader(config, 'shakespere.txt')
+# config.batch_size[0]
+# x,y = dataloader.next_batch()
+# x.shape, y.shape
     
